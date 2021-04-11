811c42690d8785fb54fd28d788082442345b0d0c
Andrew Stout <stout01@users.noreply.github.com>
2021-04-11 14:19:16 -0500

    Refactored recommended episodes to use material list

--- src/components/recommended-episodes.tsx
+++ src/components/recommended-episodes.tsx
@@ -1,19 +1,53 @@
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import ImageIcon from '@material-ui/icons/Image';
 import { Episode } from '../models/episode';
 
 type RecommendedEpisodesProps = {
   episodes?: Array<Episode>;
 };
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+  })
+);
+
 export default function RecommendedEpisodes({
   episodes,
 }: RecommendedEpisodesProps) {
+  const classes = useStyles();
+
   return (
-    <div>
+    <List className={classes.root}>
       {episodes?.map((episode) => (
-        <div>
-          {episode.episode} - {episode.name}
-        </div>
+        <ListItem key={episode.id}>
+          <ListItemAvatar>
+            <Avatar>
+              <ImageIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItem
+            button
+            component="a"
+            href={`https://rickandmorty.fandom.com/wiki/${episode.name.replaceAll(
+              ' ',
+              '_'
+            )}`}
+            target="_blank"
+          >
+            <ListItemText primary={episode.episode} secondary={episode.name} />
+          </ListItem>
+        </ListItem>
       ))}
-    </div>
+    </List>
   );
 }
