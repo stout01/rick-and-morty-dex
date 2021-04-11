61f4c5aa285de8f4249e5a98e6d1251e9f6b37cd
Andrew Stout <stout01@users.noreply.github.com>
2021-04-10 21:03:39 -0500

    Added pagination for character list

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,41 @@ import CharacterList from './components/character-list';
 import AppMenuBar from './components/app-menu-bar';
 import { CharacterResults } from './models/character-results';
 import { CharacterService } from './services/character-service';
+import Pagination from '@material-ui/lab/Pagination';
 
 function App() {
   const [characterResults, setCharacterResults] = useState<CharacterResults>();
 
   useEffect(() => {
-    async function fetchCharacter() {
-      const characterService = CharacterService.getInstance();
-
-      const charactersResponse = await characterService.getAllCharacters(1);
-
-      setCharacterResults(charactersResponse);
-    }
-
     fetchCharacter();
   }, []);
+
+  async function fetchCharacter(page: number = 1) {
+    const characterService = CharacterService.getInstance();
+
+    const charactersResponse = await characterService.getAllCharacters(page);
+
+    setCharacterResults(charactersResponse);
+  }
+
+  const handlePageChange = (
+    _event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    fetchCharacter(value);
+  };
 
   return (
     <div>
       <AppMenuBar></AppMenuBar>
       <div className="App">
         <CharacterList characterResults={characterResults}></CharacterList>
+        <Pagination
+          color="primary"
+          variant="outlined"
+          count={characterResults?.info.pages}
+          onChange={handlePageChange}
+        />
       </div>
     </div>
   );
