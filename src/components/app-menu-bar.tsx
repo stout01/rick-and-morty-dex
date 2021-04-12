4566bcc39356715d713dc5baea7f10de74dd3108
Andrew Stout <stout01@users.noreply.github.com>
2021-04-11 22:30:57 -0500

    Removed unused menu icon

--- src/components/app-menu-bar.tsx
+++ src/components/app-menu-bar.tsx
@@ -1,42 +1,10 @@
-import {
-  AppBar,
-  Button,
-  createStyles,
-  IconButton,
-  makeStyles,
-  Theme,
-  Toolbar,
-} from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Button, Toolbar } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  })
-);
-
 export default function AppMenuBar() {
-  const classes = useStyles();
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton>
         <Button color="inherit" component={Link} to="/characters">
           Characters
         </Button>
